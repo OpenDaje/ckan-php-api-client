8481f7945736fd070b8a04deb6275b18b3e71a73
zerai <teclaizerai@gmail.com>
2018-04-16 01:37:03 +0200

    cs-fix

--- src/RestClient/CkanApiClient.php
+++ src/RestClient/CkanApiClient.php
@@ -8,10 +8,11 @@ use GuzzleHttp\Command\Guzzle\GuzzleClient;
 
 class CkanApiClient extends GuzzleClient
 {
-    public static function create($config = []) {
+    public static function create($config = [])
+    {
         // Load the service description file.
         $service_description = new Description(
-            ['baseUrl' => $config['base_uri']] + (array) json_decode(file_get_contents(__DIR__ . '/../service.json'), TRUE)
+            ['baseUrl' => $config['base_uri']] + (array) json_decode(file_get_contents(__DIR__ . '/../service.json'), true)
         );
 
         // Creates the client and sets the default request headers.
@@ -23,6 +24,6 @@ class CkanApiClient extends GuzzleClient
             'auth' =>  [$config['api_user'], $config['api_pass']],
         ]);
 
-        return new static($client, $service_description, NULL, NULL, NULL, $config);
+        return new static($client, $service_description, null, null, null, $config);
     }
 }
